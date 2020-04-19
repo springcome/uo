87e3bb76efe834770bf3696a7126ad203f16744f
springcome <fun.deviation@gmail.com>
2020-04-19 20:57:41 +0900

    2020.04.18 react learn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+function Asome({name}) {
+//	console.log(prop);
+	return <h1>It is ASome {name}</h1>
+}
+
 function App() {
-  return <div>Hello</div>;
+  return (
+		  <div>
+		  	<h1>Hi, Nice meet you.</h1>
+		  	<Asome
+		  		name="Rudy" />
+		  </div>
+  );
 }
 
 export default App;
