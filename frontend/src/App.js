7e0318f4b74378f16527631282021858c7d6065d
rudy <rudy.kim@healthhub.kr>
2020-04-21 10:49:02 +0900

    테스트 게임 파일 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Game from './tictactoe/Game';
 import GridSystem from "./drafts/GridSystem";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
 		<div className="container-fluid">
-			<Game />
 			<GridSystem />
 		</div>
   );
