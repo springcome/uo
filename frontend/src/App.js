238a17c8a0a668b5094f05c87a2ab7c67afaaa23
rudy <rudy.kim@healthhub.kr>
2020-04-23 20:42:03 +0900

    Change App is function to class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import GridSystem from "./drafts/GridSystem";
 
-import "bootstrap/dist/css/bootstrap.css";
+class App extends React.Component {
+	constructor() {
+		super();
 
-function App() {
-  return (
-		<div className="container-fluid">
-			<GridSystem />
-		</div>
-  );
+		this.state = {
+			count: 0
+		}
+	}
+
+	add() {
+		console.log("1");
+		this.state.count++;
+	}
+
+	minus() {
+		console.log("2");
+		this.state.count--;
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>The number is {this.state.count}</h1>
+				<button onClick="add">Add</button>
+				<button onClick="minus">Minus</button>
+			</div>
+		);
+	}
 }
 
 export default App;
